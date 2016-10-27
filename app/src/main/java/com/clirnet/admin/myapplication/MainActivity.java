75ba4a7bdbf7bf60b15ab121ad1ec9e48f0bc8fe
ashishuclirnet <ashish007>
2016-10-27 13:36:20 +0530

    data updated

--- app/src/main/java/com/clirnet/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/clirnet/admin/myapplication/MainActivity.java
@@ -21,92 +21,63 @@ import com.gitonway.lee.niftymodaldialogeffects.lib.NiftyDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
-    WebView webView;
 
+    private Effectstype effect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.fullscreen_content);
-
-        webView = (WebView)findViewById(R.id.fullscreen_content);
-        String apikey=getResources().getString(R.string.apikey);
-           Log.e("apikey",""+apikey);
-        webView.setWebViewClient(new MyWebViewClient());
-        webView.loadUrl("file:///android_asset/www/index.html");
-        //you can also link to a website. Example:
-        //webView.loadUrl("www.google.com");
-        //I have included web permissions in the AndroidManifest.xml
-        webView.setWebChromeClient(new WebChromeClient());
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setDatabasePath("/data/data/"+this.getPackageName()+"/databases/");
-
-        Button openDialogButton = (Button)findViewById(R.id.button1);
-
-        openDialogButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                NiftyDialogBuilder dialogBuilder= NiftyDialogBuilder.getInstance(getApplicationContext());
-
-                dialogBuilder
-                        .withTitle("Modal Dialog")                                  //.withTitle(null)  no title
-                        .withTitleColor("#FFFFFF")                                  //def
-                        .withDividerColor("#11000000")                              //def
-                        .withMessage("This is a modal Dialog.")                     //.withMessage(null)  no Msg
-                        .withMessageColor("#FFFFFFFF")                              //def  | withMessageColor(int resid)
-                        .withDialogColor("#FFE74C3C")                               //def  | withDialogColor(int resid)
-                        .withIcon(getResources().getDrawable(R.drawable.languge))
-                        .withDuration(700)                                          //def
-                        .withEffect(Effectstype.Fall)                                         //def Effectstype.Slidetop
-                        .withButton1Text("OK")                                      //def gone
-                        .withButton2Text("Cancel")                                  //def gone
-                        .isCancelableOnTouchOutside(true)                           //def    | isCancelable(true)
-                        .setCustomView(R.layout.custom_view,v.getContext())         //.setCustomView(View or ResId,context)
-                        .setButton1Click(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                Toast.makeText(v.getContext(), "i'm btn1", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .setButton2Click(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                Toast.makeText(v.getContext(),"i'm btn2",Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .show();
-                /*final Dialog openDialog = new Dialog(MainActivity.this);
-                openDialog.setContentView(R.layout.cust_dialog);
-                //openDialog.setTitle("Custom Dialog Box");
-                TextView dialogTextContent = (TextView)openDialog.findViewById(R.id.dialog_text);
-                ImageView dialogImage = (ImageView)openDialog.findViewById(R.id.dialog_image);
-                Button dialogCloseButton = (Button)openDialog.findViewById(R.id.dialog_button);
-                dialogCloseButton.setOnClickListener(new OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        // TODO Auto-generated method stub
-                        openDialog.dismiss();
-                    }
-                });
-                openDialog.show();*/
-            }
-
-        });
-
 
     }
 
-    @Override
-    public void onBackPressed()
-    {
-        if(webView.canGoBack())
-            webView.goBack();
-        else
-            super.onBackPressed();
+    public void dialogShow(View v){
+        NiftyDialogBuilder dialogBuilder=NiftyDialogBuilder.getInstance(this);
+
+        switch (v.getId()){
+            case R.id.fadein:effect=Effectstype.Fadein;break;
+            case R.id.slideright:effect=Effectstype.Slideright;break;
+            case R.id.slideleft:effect=Effectstype.Slideleft;break;
+            case R.id.slidetop:effect=Effectstype.Slidetop;break;
+            case R.id.slideBottom:effect=Effectstype.SlideBottom;break;
+            case R.id.newspager:effect=Effectstype.Newspager;break;
+            case R.id.fall:effect=Effectstype.Fall;break;
+            case R.id.sidefall:effect=Effectstype.Sidefill;break;
+            case R.id.fliph:effect=Effectstype.Fliph;break;
+            case R.id.flipv:effect=Effectstype.Flipv;break;
+            case R.id.rotatebottom:effect=Effectstype.RotateBottom;break;
+            case R.id.rotateleft:effect=Effectstype.RotateLeft;break;
+            case R.id.slit:effect=Effectstype.Slit;break;
+            case R.id.shake:effect=Effectstype.Shake;break;
+        }
+
+        dialogBuilder
+                .withTitle("Modal Dialog")                                  //.withTitle(null)  no title
+                .withTitleColor("#FFFFFF")                                  //def
+                .withDividerColor("#11000000")                              //def
+                .withMessage("This is a modal Dialog.")                     //.withMessage(null)  no Msg
+                .withMessageColor("#FFFFFFFF")                              //def  | withMessageColor(int resid)
+                .withDialogColor("#FFE74C3C")                               //def  | withDialogColor(int resid)                               //def
+                .withIcon(getResources().getDrawable(R.drawable.languge))
+                .isCancelableOnTouchOutside(true)                           //def    | isCancelable(true)
+                .withDuration(700)                                          //def
+                .withEffect(effect)                                         //def Effectstype.Slidetop
+                .withButton1Text("OK")                                      //def gone
+                .withButton2Text("Cancel")                                  //def gone
+                .setCustomView(R.layout.custom_view,v.getContext())         //.setCustomView(View or ResId,context)
+                .setButton1Click(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(v.getContext(), "i'm btn1", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setButton2Click(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(v.getContext(), "i'm btn2", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
     }
 
 
